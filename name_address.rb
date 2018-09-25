fabca0f1da082cd8208a2b719fdd9e6668ab85cc
April atkins <aprilatkins@Fawns-MacBook-Pro.local>
2018-09-25 11:38:59 -0400

    address

---
+++ name_address.rb
@@ -0,0 +1,9 @@
+def my_name_and_address(fname,address,phone) 
+	puts "name: " + fname
+	puts "address: #{address}"
+	puts "phone: " + phone.to_s
+end
+my_name_and_address("Fawn","100 Hamon", 3043345676) 
+my_name_and_address("MArv","123 Elm", "3045567890") 
+my_name_and_address("Jack","1 River", "3045553938") 
+my_name_and_address("Fawn","100 Hamon","3045458214") 
