3f2f8d68c46713c1679960c4c5ce894f40a17875
April atkins <aprilatkins@Fawns-MacBook-Pro.local>
2018-09-25 10:43:37 -0400

    mined minds

--- loop_to_100.rb
+++ loop_to_100.rb
@@ -1,6 +1,9 @@
 1.upto(100) do |x|
-	if x % 3 == 0 
-		puts"mined"
+	if x % 3 == 0 && x % 5 == 0
+		puts "mined minds"
+	elsif 
+		x % 3 == 0 
+		puts "mined"
     elsif 
     	x % 5 == 0
     	puts "minds" 
